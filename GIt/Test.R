e811f57eb506d8dad0789e8ab77fdbffa63d7090
Arash Bagherabadi <arash.bagherabadi@gmail.com>
2024-08-12 03:15:51 +0330

    added plot code

--- GIt/Test.R
+++ GIt/Test.R
@@ -1,1 +1,16 @@
 print("Hello")
+
+# Sample data
+x <- c(1, 2, 3, 4, 5)
+y <- c(10, 20, 25, 30, 35)
+
+# Create the plot
+plot(x, y, type = "o", col = "blue", 
+     xlab = "X-axis", ylab = "Y-axis", 
+     main = "Simple Plot")
+
+# Add a grid
+grid()
+)
+
+R.home("bin") #"C:/PROGRA~1/R/R-44~1.1/bin/x64"
